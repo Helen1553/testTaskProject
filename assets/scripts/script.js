7d62e6dc6f11b57c7f06f325f6eaa0c1397aedb8
Helen1553 <alena.tyulinaa@gmail.com>
2025-05-09 09:56:57 +0300

    added mobile & tablet version _ added burger-menu

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+//Функция для выплывающего бургер-меню
+const burger = document.querySelector("#burger");
+const popup = document.getElementById("popup");
+const body = document.body;
+
+const menu = document.getElementById("menu").cloneNode(1);
+
+burger.addEventListener("click", burgerHandler);
+
+function burgerHandler(e) {
+    e.preventDefault();
+
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+const renderPopup = () => {
+popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+const closeOnClick = () => {
+    popup.classList.remove("open");
+    burger.classList.remove("active");
+    body.classList.remove("noscroll");
+}
